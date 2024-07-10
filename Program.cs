a7aa3da86d21f6a754986bf360182f52c7233003
Oksana <okstrishch@gmail.com>
2024-07-10 23:54:41 +0300

    Added second version calculator

--- Program.cs
+++ Program.cs
@@ -10,27 +10,30 @@ namespace HW2_Calculator
             var firstNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter second number");
             var secondNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter operation");
+            Console.WriteLine("Please choose operation *, /, +, -");
             var operation = Console.ReadLine();
             Console.WriteLine($"Result is {firstNum} {operation} {secondNum}: {Operation(operation, firstNum, secondNum)}");
         }
 
         static int Operation(string option, int firstNum, int secondNum)
         {
-            if (option == "+") { return firstNum + secondNum; }
-            else if (option == "/")
+            int result = 0;
+            switch (option)
             {
-                return firstNum / secondNum;
+                case "+": result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                case "*":
+                    result = firstNum * secondNum;
+                    break;
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+            }
 
-            }
-            else if (option == "*")
-            {
-                return firstNum * secondNum;
-            }
-            else
-            {
-                return firstNum - secondNum;
-            }
+            return result;
         }
     }
 }
