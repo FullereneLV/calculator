8141aeac0668a4c522cc83b8f0ccb20db8ace8b2
Oksana <okstrishch@gmail.com>
2024-07-11 00:14:41 +0300

    added calculator3

--- Program.cs
+++ Program.cs
@@ -6,13 +6,23 @@ namespace HW2_Calculator
     {
         static void Main(string[] args)
         {
+            string value;
+            do { AskNumbers();
+                Console.Write("Do you want to continue(y/n):");
+                value = Console.ReadLine();
+            } while (value == "y" || value == "Y");
+
+        }
+
+        static void AskNumbers()
+        {
             Console.WriteLine("Please enter first number");
             var firstNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter second number");
             var secondNum = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please choose operation *, /, +, -");
             var operation = Console.ReadLine();
-            Console.WriteLine($"Result is {firstNum} {operation} {secondNum}: {Operation(operation, firstNum, secondNum)}");
+            Console.WriteLine($"Result is {firstNum} {operation} {secondNum}= {Operation(operation, firstNum, secondNum)}");
         }
 
         static int Operation(string option, int firstNum, int secondNum)
